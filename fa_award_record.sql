fff5d97e220ff276361809460a1a24be33c6a777
JamesCurtis <jamescurtis@foxmail.com>
2022-10-27 11:13:11 +0800

    feat: 允许重复获取邀请奖励设置
    link redrose-office/digital-collections#119

--- fa_award_record.sql
+++ fa_award_record.sql
@@ -14,3 +14,6 @@ create table fa_award_record
 INSERT INTO bowen.fa_award_record (id, uid, award_id, goods_id, price, create_time, orderNo) VALUES (13608, 30699, 1, 95, 68.00, '2022-10-18 17:09:25', null);
 INSERT INTO bowen.fa_award_record (id, uid, award_id, goods_id, price, create_time, orderNo) VALUES (13609, 30690, 31, 95, 68.00, '2022-10-18 17:10:15', null);
 INSERT INTO bowen.fa_award_record (id, uid, award_id, goods_id, price, create_time, orderNo) VALUES (13610, 30690, 31, 95, 68.00, '2022-10-18 17:10:28', null);
+INSERT INTO bowen.fa_award_record (id, uid, award_id, goods_id, price, create_time, orderNo) VALUES (13611, 30690, 31, 50, 1.00, '2022-10-27 11:00:39', null);
+INSERT INTO bowen.fa_award_record (id, uid, award_id, goods_id, price, create_time, orderNo) VALUES (13612, 30690, 32, 95, 68.00, '2022-10-27 11:00:39', null);
+INSERT INTO bowen.fa_award_record (id, uid, award_id, goods_id, price, create_time, orderNo) VALUES (13613, 30690, 32, 95, 68.00, '2022-10-27 11:07:24', null);
